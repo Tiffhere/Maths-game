2eb8697373697fe90f27dbd85a540fb1cd5590b9
xiaopow <xiaoyang88@gmail.com>
2021-03-16 13:00:10 +0800

    xiao fixes

--- application.js
+++ application.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
   var timeLeft = 10;
   var score = 0;
   var interval;
-  var rangeValue;
+  var rangeValue = 10;
+  var selectedQuestionType = 'add';
 
   var randomNumberGenerator = function (size) {
     return Math.ceil(Math.random() * size);
@@ -14,96 +15,105 @@ $(document).ready(function(){
     var num1 = randomNumberGenerator(rangeValue);
     var num2 = randomNumberGenerator(rangeValue);
 
-    $(".plus").click(function(){
-      question.answer = num1 + num2;
-      question.equation = String(num1) + " + " + String(num2);
-    });
-
-    $(".minus").click(function(){
-      question.answer = num1 - num2;
-      question.equation = String(num1) + " - " + String(num2);
-    });
-
-    $(".multiply").click(function(){
-      question.answer = num1 * num2;
-      question.equation = String(num1) + " * " + String(num2);
-    });
-
-    $(".divide").click(function(){
-      question.answer = num1 / num2;
-      question.equation = String(num1) + " / " + String(num2);
-    });
-
-
+    switch (selectedQuestionType) {
+      case 'add':
+        question.answer = num1 + num2;
+        question.equation = String(num1) + " + " + String(num2);
+        break;
+      case 'minus':
+        if (num1 > num2) {
+          question.answer = num1 - num2;
+          question.equation = String(num1) + " - " + String(num2);
+        } else {
+          question.answer = num2 - num1;
+          question.equation = String(num2) + " - " + String(num1);
+        }
+        break;
+      case 'multiply':
+        question.answer = num1 * num2;
+        question.equation = String(num1) + " * " + String(num2);
+        break;
+      case 'divide':
+        var num3 = num1 * num2;
+        question.answer = num1;
+        question.equation = String(num3) + " / " + String(num2);
+        break;
+    }
 
     return question;
   }
+
+  $('button.qType').on('click', function () {
+    var newType = $(this).attr('name');
+    selectedQuestionType = newType;
+    $('#questionType').text(newType);
+  })
 
   var renderNewQuestion = function () {
     currentQuestion = questionGenerator();
     $('#equation').text(currentQuestion.equation);
   }
 
-    var checkAnswer = function (userInput, answer){
-      if(userInput === answer) {
-        renderNewQuestion();
-        $('#user-input').val('');
-        updateTimeLeft(+1);
+  var checkAnswer = function (userInput, answer){
+    if(userInput === answer) {
+      renderNewQuestion();
+      $('#user-input').val('');
+      updateTimeLeft(+1);
 
-      }
     }
+  }
 
-    var updateScre = function(amount){
-      score += amount;
-      document.getElementById("score").innerHTML = score;
-    };
+  var updateScre = function(amount){
+    score += amount;
+    document.getElementById("score").innerHTML = score;
+  };
 
-    $('#user-input').on('keyup', function(){
-      checkAnswer(Number($(this).val()),currentQuestion.answer);
-    });
+  $('#user-input').on('keyup', function(){
+    checkAnswer(Number($(this).val()),currentQuestion.answer);
+  });
 
-    renderNewQuestion();
+  renderNewQuestion();
 
-
-
-    var startGame = function(){
-      if(!interval){
-        interval = setInterval(function(){
-          updateTimeLeft(-1);
-          if (timeLeft === 0){
-            clearInterval(interval);
-            interval = undefined;
-          }
-        }, 1000);
-      }
+  var startGame = function(){
+    if(!interval){
+      interval = setInterval(function(){
+        updateTimeLeft(-1);
+        if (timeLeft === 0){
+          clearInterval(interval);
+          interval = undefined;
+        }
+      }, 1000);
     }
+  }
 
-    $('#user-input').on('keyup', function(){
-      startGame();
-      checkAnswer(Number($(this).val()),
+  $('#user-input').on('keyup', function(){
+    startGame();
+    checkAnswer(Number($(this).val()),
     currentQuestion.answer);
   });
 
 
-    var updateTimeLeft = function(amount){
-      timeLeft += amount;
-      $('#time-left').text(timeLeft);
-    }
+  var updateTimeLeft = function(amount){
+    timeLeft += amount;
+    $('#time-left').text(timeLeft);
+  }
 
-    $('input[type="range"]').on('input',()=> {
-      var control = $(this),
-          controlMin = control.attr('min'),
-          controlMax = control.attr('max'),
-          controlVal = control.val(),
-          controlThumbWidth = control.data('thumbwidth');
+  $('#inputRange').on('change', function() {
+    var control = $(this),
+        // controlMin = control.attr('min'),
+        // controlMax = control.attr('max'),
+        controlVal = control.val(),
+        // controlThumbWidth = control.data('thumbwidth');
+    
+    rangeValue = controlVal;
+    
+    // var range = controlMax - controlMin;
 
-      var range = controlMax - controlMin
-
-      var position = ((controlVal-controlMin)/range)*100;
-      var positionOffset = Math.round(controlThumbWidth*position/100)-(controlThumbWidth/2);
-      var output = control.next('output');
-      output.css('left', 'calc(' + position + '% - ' + positionOffset + 'px)').text(controlVal);
-    });
+    // var position = ((controlVal-controlMin)/range)*100;
+    // var positionOffset = Math.round(controlThumbWidth*position/100)-(controlThumbWidth/2);
+    var output = $('#rangeVal');
+    output.text(controlVal);
+  });
 
 
 
