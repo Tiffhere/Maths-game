4334295c65c564bfb2ae483b200550088e588f54
Tiffany <tiffanyhere01@gmail.com>
2021-03-19 13:32:04 +0800

    updated the question

--- application.js
+++ application.js
@@ -46,6 +46,7 @@ $(document).ready(function(){
   $('button.qType').on('click', function () {
     var newType = $(this).attr('name');
     selectedQuestionType = newType;
+    renderNewQuestion();
     $('#questionType').text(newType);
   })
 
